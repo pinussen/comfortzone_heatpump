5014215c750f39b9140f6591478d32b134a340b1
Bjorn Wallentinus <bjorn.wallentinus@gmail.com>
2026-03-05 14:51:28 +0100

    Guard Arduino logging for ESP-IDF builds

--- comfortzone_config.h
+++ comfortzone_config.h
@@ -20,10 +20,15 @@
 
 #define COMFORTZONE_HEATPUMP_LAST_MESSAGE_BUFFER_SIZE 256
 
-#ifdef USE_ESPHOME
+#if defined(USE_ESPHOME) && defined(ARDUINO)
 #include <Arduino.h>
 #define DPRINT(...) Serial.printf("[D][comfortzone] " __VA_ARGS__)
-#define DPRINTLN(...) DPRINT(__VA_ARGS__); Serial.println()
+#define DPRINTLN(...) do { DPRINT(__VA_ARGS__); Serial.println(); } while (0)
+#elif defined(USE_ESPHOME)
+#include "esphome/core/log.h"
+static const char *TAG = "comfortzone";
+#define DPRINT(args...)   ESP_LOGD(TAG, args)
+#define DPRINTLN(args...) ESP_LOGD(TAG, args)
 #endif
 
 #if defined(DEBUG) && !defined(USE_ESPHOME)
